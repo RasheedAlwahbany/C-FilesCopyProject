ece0ac0861b1472b7ecb5b2f2e9ba028f3c4a4c1
Rasheed Alwahbany <rasheedalwahbany@gmail.com>
2023-02-10 11:34:16 +0300

    Upload project

---
+++ Copy_Files/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Copy_Files
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.progress_copy = new System.Windows.Forms.ProgressBar();
+            this.toolStrip2 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tool_txt_from_path = new System.Windows.Forms.TextBox();
+            this.tool_txt_to_path = new System.Windows.Forms.TextBox();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // progress_copy
+            // 
+            resources.ApplyResources(this.progress_copy, "progress_copy");
+            this.progress_copy.Name = "progress_copy";
+            // 
+            // toolStrip2
+            // 
+            resources.ApplyResources(this.toolStrip2, "toolStrip2");
+            this.toolStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton2,
+            this.toolStripButton1});
+            this.toolStrip2.Name = "toolStrip2";
+            // 
+            // toolStripButton2
+            // 
+            resources.ApplyResources(this.toolStripButton2, "toolStripButton2");
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tool_txt_to_path);
+            this.panel1.Controls.Add(this.tool_txt_from_path);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Name = "panel1";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // tool_txt_from_path
+            // 
+            resources.ApplyResources(this.tool_txt_from_path, "tool_txt_from_path");
+            this.tool_txt_from_path.Name = "tool_txt_from_path";
+            this.tool_txt_from_path.Click += new System.EventHandler(this.textBox1_Click);
+            // 
+            // tool_txt_to_path
+            // 
+            resources.ApplyResources(this.tool_txt_to_path, "tool_txt_to_path");
+            this.tool_txt_to_path.Name = "tool_txt_to_path";
+            this.tool_txt_to_path.Click += new System.EventHandler(this.textBox2_Click);
+            // 
+            // toolStripButton1
+            // 
+            resources.ApplyResources(this.toolStripButton1, "toolStripButton1");
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click_1);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.toolStrip2);
+            this.Controls.Add(this.progress_copy);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip2.ResumeLayout(false);
+            this.toolStrip2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ProgressBar progress_copy;
+        private System.Windows.Forms.ToolStrip toolStrip2;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tool_txt_to_path;
+        private System.Windows.Forms.TextBox tool_txt_from_path;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+    }
+}
+
---
+++ Copy_Files/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Copy_Files
+{
+    public partial class Form1 : Form
+    {
+        string file_name = "",file_name2="";
+        int file_size=0,counter=1;
+        public void copy_files()
+        {
+            file_size = 100 / (file_size + 1);
+            file_name = file_name2;
+            progress_copy.Value = 0;
+            try
+            {
+                string str = "";
+                char chars = ' ';
+                string name = file_name + "=";
+                for (int i = 0; i < name.Length; i++)
+                {
+                    if (name[i] == '=')
+                        break;
+                    else if (chars == '.')
+                        str = str + name[i];
+
+                    else
+                        chars = name[i];
+                }
+                // MessageBox.Show();
+                string[] array = { "txt", "text", "php", "sql", "html", "js", "css", "db", "doc", "cs", "cpp", "h", "dll" };
+                if (array.Contains(str.ToLower()))
+                {
+
+                    StreamReader reads = new StreamReader(tool_txt_from_path.Text);
+
+                    string error = "no";
+
+                    if (File.Exists(tool_txt_to_path.Text + file_name))
+                    {
+                        DialogResult dialog = MessageBox.Show(this, "File Exist You want to replace it:", "Info", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Information);
+                        if (dialog == DialogResult.Yes)
+                        {
+                            File.Delete(tool_txt_to_path.Text + file_name);
+
+                        }
+                        else if (dialog == DialogResult.No)
+                        {
+                            counter = 1;
+
+                            file_name = files_name_changes(file_name2);
+
+                            while (File.Exists(tool_txt_to_path.Text + file_name))
+                            {
+                                file_name = files_name_changes(file_name2);
+                            }
+                        }
+                        else
+                        {
+                            error = "yes";
+                        }
+
+                    }
+                    if (error != "yes")
+                    {
+                        StreamWriter writer = new StreamWriter(tool_txt_to_path.Text + file_name, true);
+                        while (reads.Peek() >= 0)
+                        {
+
+                            writer.WriteLine(reads.ReadLine());
+                            if (progress_copy.Value + file_size < 100)
+                                progress_copy.Value = progress_copy.Value + file_size;
+                            else
+                                progress_copy.Value = 100;
+
+                        }
+                        progress_copy.Value = 100;
+                        writer.Close();
+                    }
+                    //string str = reads.ReadToEnd();
+
+
+
+                    reads.Close();
+                }
+                else
+                {
+                    File.Copy(tool_txt_from_path.Text, tool_txt_to_path.Text + file_name, true);
+                    progress_copy.Value = 100;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("" + ex.Message);
+            }
+        }
+        public string files_name_changes(string name)
+        {
+            string name2="",name_ext="";
+            char ext=' ';
+            for(int i=0; i< name.Length; i++)
+            {
+                if (name[i] == '.')
+                    ext = '.';
+                if (ext == '.')
+                {
+                    name_ext = name_ext + name[i];
+                }
+                else
+                    name2 = name2 + name[i];
+                   
+            }
+
+            name2 = name2 + "(copy " + counter + ")" + name_ext;
+            counter += 1;
+            return name2; 
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripTextBox1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void tool_txt_to_path_Click(object sender, EventArgs e)
+        {
+
+            
+
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog file = new OpenFileDialog();
+
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                //string[] files = file.FileNames;
+                int count = 0;
+
+                string[] str_array = new string[10];
+                string str = "", files = file.FileName + "\\";
+                for (int i = 0; i < files.Length; i++)
+                {
+                    if (files[i] == '\\')
+                    {
+                        str_array[count] = str;
+                        count += 1;
+                        str = "";
+                    }
+                    else
+                    {
+                        str = str + files[i];
+                    }
+
+
+                }
+
+                file_name = str_array[count - 1];
+                file_name2 = file_name;
+                // tool_txt_to_path.Text = str_array[count-1];
+                tool_txt_from_path.Text = file.FileName;
+                StreamReader reads = new StreamReader(file.FileName);
+                while (reads.Peek() >= 0)
+                {
+                    reads.ReadLine();
+                    file_size += 1;
+                }
+                reads.Close();
+
+            }
+        }
+
+        private void textBox2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+
+            if (folder.ShowDialog() == DialogResult.OK)
+            {
+                //MessageBox.Show(folder.SelectedPath);
+                tool_txt_to_path.Text = folder.SelectedPath + "\\";
+
+            }
+        }
+
+        private void toolStripButton1_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show(this,"This is App under RAGT2019 Copy Rights\nThanks For Visit us.\nEng.Rasheed Adnan Al-Wahbany ^_^","Info",MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
+
+        private void toolStripTextBox2_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            if(tool_txt_from_path.Text!=""&&tool_txt_to_path.Text!="")
+            copy_files();
+        }
+    }
+}
